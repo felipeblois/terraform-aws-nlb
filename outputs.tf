8161b54d57d07d0a9124acb023c8d1d21b3a8582
felipeblois <93614487+felipeblois@users.noreply.github.com>
2024-03-24 17:11:15 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,8 @@ output "nlb_dns_name" {
   description = "The DNS name of the NLB"
   value       = aws_lb.network.dns_name
 }
+
+output "nlb_arn" {
+  description = "ARN do Network Load Balancer criado"
+  value       = aws_lb.network.arn
+}
